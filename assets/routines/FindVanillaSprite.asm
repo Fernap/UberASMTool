947ffc887085203b462fca874a3ed6d8a2d3bac3
Fernap <fernap@github>
2023-11-12 17:43:41 -0500

    Fix/add routines for finding sprites

--- assets/routines/FindVanillaSprite.asm
+++ assets/routines/FindVanillaSprite.asm
@@ -1,8 +1,9 @@
-; FindSprite - tries to find the sprite slot occupied by the given sprite number
+; FindVanillaSprite - tries to find the non-custom sprite slot occupied by the given sprite number
+;                     Pixi must be installed in order to call this
 ;
 ; Input:
 ;     X   : sprite slot to begin searching in, plus 1 (searches downwards, safe to pass $00, which will always result in no match)
-;     $00 : the sprite number to search for (will use the "act as" setting for custom sprites)
+;     $00 : the sprite number to search for (custom sprites will be ignored)
 ;
 ; Output:
 ;     X : sprite slot containing the sprite (which might be in a dead state), or $FF is not found
@@ -17,6 +18,9 @@
     beq ?-
     lda !sprite_num,x
     cmp $00
+    bne ?-
+    lda !extra_bits,x
+    and #$08
     bne ?-
 
 ?.found:
